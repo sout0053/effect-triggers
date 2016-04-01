3a5ae3c7b1f6eedf422b62300583b6a4fd229e8b
sout0053 <maxwell.southwood@outlook.com>
2016-04-01 09:53:16 -0400

    Created variables

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var $body = $('body');
+
+var $btnShowHide = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnColEx = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
